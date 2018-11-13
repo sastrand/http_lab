a075b17358a28ff85bf804eea27da570af200866
sascha <sastrand@pdx.edu>
2018-11-13 12:23:32 -0800

    fixed typo

--- main.py
+++ main.py
@@ -14,13 +14,8 @@ lang_abbr_map = create_lang_abbr_map()
 
 
 def get_advice():
-    advice = list()
-
-    for _ in range(0, 50):
-        r = requests.get('https://api.adviceslip.com/advice')
-        advice.append(str(r.json()))
-
-    print(f"most common advice: {find_most_popular_advice(advice)}")
+    pass
+    # remove `pass` and [FILL IN HERE]
 
 
 def find_most_popular_advice(advice_list):
@@ -44,23 +39,13 @@ def format_lang_string(lang_str):
 
 
 def post_language_samples():
-    headers = {"Authorization": "Bearer 80b4ffd09ed8d997433f36a621dc9d62"}
-
     lang_samples = ["Përshendetje Botë", "Русский компьютер", "Ahoj světe",
                     "ਕੰਪਿਊਟਰ ਵਿਗਿਆਨ", "مرحبا بالعالم", "Labas pasauli",
                     "Привет мир", "ਯੇਅ ਨਿਊ ਸ਼ੁਰੂਆਤ", "ہیلو دنیا",
                     "Salam dünya", "नमस्कार संसार", "Halló heimur",
                     "Беларускія Кампутарныя", "ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ ਦੁਨਿਆ",
                     "Прывітанне Сусвет"]
-
-    lang_results = list()
-    for ls in lang_samples:
-        r = requests.post('https://ws.detectlanguage.com/0.2/detect',
-                          headers=headers, params=format_lang_string(ls))
-        abbr = get_lang_abbr_from_resp(r)
-        lang_results.append((ls, lang_abbr_map[abbr]))
-
-    return lang_results
+    # [FILL IN HERE]
 
 
 def get_lang_abbr_from_resp(http_resp):
@@ -72,13 +57,7 @@ def get_lang_abbr_from_resp(http_resp):
     return http_resp.json()["data"]["detections"][0]["language"]
 
 
-def find_language_appearing_most(lang_results):
-    abbrs = [x[1] for x in lang_results]
-    most_freq_abbr = max(abbrs, key=lambda x: abbrs.count(x))
-    return [x for x in lang_results if x[1] == most_freq_abbr]
-
-
 if __name__ == "__main__":
+    pass
+    # Remove `pass` and [FILL IN HERE]
 
-    get_advice()
-    print(find_language_appearing_most(post_language_samples()))
